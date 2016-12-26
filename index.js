e5c66911d910ee7c4e0cdbff138e38130e8ae2c0
noby <n.fujioka@abacusmaths.info>
2016-12-26 03:09:14 +0000

    c

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.get('/*', function(request, response) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
